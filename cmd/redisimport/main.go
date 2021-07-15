758ceb899b4d3b843425673c1af7f369ff7ff822
Tobias von Dewitz <tobias@scraperwall.com>
2021-07-15 21:42:52 +0200

    made redis importer threaded

--- cmd/redisimport/main.go
+++ cmd/redisimport/main.go
@@ -8,18 +8,157 @@ import (
 	"image/jpeg"
 	"log"
 	"path/filepath"
+	"sync"
 	"time"
 
-	"github.com/cheggaaa/pb/v3"
 	"github.com/davidbyttow/govips/v2/vips"
 	redis "github.com/go-redis/redis/v8"
 )
+
+type Importer struct {
+	Label    string
+	Tilesize int
+	Redis    *redis.Client
+	Time     time.Duration
+	Workers  int
+	Total    int
+	Current  int
+	wg       sync.WaitGroup
+	mutex    sync.Mutex
+}
+
+func NewImporter(label string, tilesize int, redisAddr string, workers int) (*Importer, error) {
+	i := Importer{
+		Label:    label,
+		Tilesize: tilesize,
+		Time:     0,
+		Redis:    redis.NewClient(&redis.Options{Addr: redisAddr}),
+		Workers:  workers,
+		Current:  0,
+		mutex:    sync.Mutex{},
+		wg:       sync.WaitGroup{},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	res := i.Redis.Ping(ctx)
+	if res.Err() != nil {
+		return nil, res.Err()
+	}
+
+	return &i, nil
+}
+
+func (i *Importer) Worker(filenameChan chan string) {
+	for fn := range filenameChan {
+		i.Import(fn)
+	}
+}
+
+func (i *Importer) AddToTime(d time.Duration) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+	i.Time += d
+}
+
+func (i *Importer) Progress() {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+
+	i.Current++
+
+	if i.Current%100 == 0 {
+		log.Printf("%d/%d (%.2f%%)\n", i.Current, i.Total, float64(i.Current*100)/float64(i.Total))
+	}
+}
+
+func (i *Importer) Run(glob string) error {
+	images, err := filepath.Glob(glob)
+	if err != nil {
+		return err
+	}
+
+	i.mutex.Lock()
+	i.Total = len(images)
+	i.mutex.Unlock()
+
+	fnameChan := make(chan string)
+	for x := 0; x < i.Workers; x++ {
+		go i.Worker(fnameChan)
+	}
+
+	for _, filename := range images {
+		i.Progress()
+		fnameChan <- filename
+	}
+	close(fnameChan)
+	i.wg.Wait()
+	return nil
+}
+
+func (i *Importer) Import(filename string) {
+	tStart := time.Now()
+	img, err := vips.NewImageFromFile(filename)
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+		return
+	}
+
+	// remove a white frame around the picture
+	left, top, width, height, err := img.FindTrim(40, &vips.Color{R: 255, G: 255, B: 255})
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+	}
+
+	if width < img.Width() || height < img.Height() {
+		err = img.ExtractArea(left, top, width, height)
+		if err != nil {
+			log.Printf("%s: %s\n", filename, err)
+		}
+	}
+
+	err = img.Thumbnail(i.Tilesize, i.Tilesize, vips.InterestingCentre)
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+		return
+	}
+
+	avg, err := img.Average()
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+		return
+	}
+
+	image, err := img.ToImage(vips.NewDefaultPNGExportParams())
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+		return
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	err = jpeg.Encode(buf, image, &jpeg.Options{Quality: 90})
+	if err != nil {
+		log.Printf("%s: %s\n", filename, err)
+		return
+	}
+
+	i.AddToTime(time.Now().Sub(tStart))
+
+	k := fmt.Sprintf("%s:%d:%d:%s", i.Label, i.Tilesize, int(avg), filepath.Base(filename))
+
+	res := i.Redis.Set(context.Background(), k, buf.Bytes(), 0)
+	if res.Err() != nil {
+		log.Printf("%s: %s\n", filename, res.Err())
+	}
+
+}
 
 func main() {
 	var tileGlob = flag.String("tileglob", "", "import all images that match this glob pattern")
 	var label = flag.String("label", "gosaic", "save the tiles using this label")
 	var tileSize = flag.Int("tilesize", 100, "crop and scale the tiles to this size")
 	var redisAddr = flag.String("redisaddr", "localhost:6379", "import the images into this redis instance")
+	var workers = flag.Int("workers", 8, "the number of parallel import workers")
 
 	flag.Parse()
 
@@ -27,78 +166,15 @@ func main() {
 		log.Println(message)
 	}, vips.LogLevelError)
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr: *redisAddr,
-	})
-
-	images, err := filepath.Glob(*tileGlob)
+	imp, err := NewImporter(*label, *tileSize, *redisAddr, *workers)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bar := pb.StartNew(len(images))
-
-	tLoad := time.Duration(0)
-
-	for _, filename := range images {
-		bar.Increment()
-		// log.Println(filename)
-
-		tStart := time.Now()
-		img, err := vips.NewImageFromFile(filename)
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-			continue
-		}
-
-		// remove a white frame around the picture
-		left, top, width, height, err := img.FindTrim(40, &vips.Color{R: 255, G: 255, B: 255})
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-		}
-
-		if width < img.Width() || height < img.Height() {
-			err = img.ExtractArea(left, top, width, height)
-			if err != nil {
-				log.Printf("%s: %s\n", filename, err)
-			}
-		}
-
-		err = img.Thumbnail(*tileSize, *tileSize, vips.InterestingCentre)
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-			continue
-		}
-
-		avg, err := img.Average()
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-			continue
-		}
-
-		image, err := img.ToImage(vips.NewDefaultPNGExportParams())
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-			continue
-		}
-
-		buf := bytes.NewBuffer([]byte{})
-		err = jpeg.Encode(buf, image, &jpeg.Options{Quality: 90})
-		if err != nil {
-			log.Printf("%s: %s\n", filename, err)
-			continue
-		}
-		tLoad += time.Now().Sub(tStart)
-
-		k := fmt.Sprintf("%s:%d:%d:%s", *label, *tileSize, int(avg), filepath.Base(filename))
-
-		res := rdb.Set(context.Background(), k, buf.Bytes(), 0)
-		if res.Err() != nil {
-			log.Printf("%s: %s\n", filename, res.Err())
-		}
+	err = imp.Run(*tileGlob)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	bar.Finish()
-
-	fmt.Printf("load time: %s\n", tLoad)
+	fmt.Printf("load time: %s\n", imp.Time)
 }
